26ec1f13f2aecb3519aab8daba3a1ac4063ef194
Islane Cavalcante <nanewjmorais@icloud.com>
2024-12-10 13:44:33 -0300

    Atualizações  na estrutura do projeto e páginas do site

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,17 @@
 from app import app #Está importando o objeto app do módulo app. Representa a aplicação.
 from flask import render_template #Está importando a função para carregar e renderizar arquivos html.
 
+# Cria uma rota para a URL base ("/"), que será a página inicial do site.
 @app.route('/') #Essa linha cria uma rota, uma url onde a página está acessível.
 def home(): #Define uma função chamada ()home. Será executada quando o usuário acessar a página inicial.
     return render_template('home.html') #Diz ao flask para renderizar o arquivo home.html e enviar ao navegador do usuário. 
  
-@app.route('/sobre')
+# Cria uma rota para a URL "/sobre".
+@app.route('/sobre')  # Define a função `sobre`, que será executada ao acessar a página Sobre.
 def sobre():
     return render_template('sobre.html')
 
+# Cria uma rota para a URL "/produtos", que será a página de demonstração dos produtos.
 @app.route('/produtos')
 def produtos():
     return render_template('produtos.html')
@@ -21,5 +24,6 @@ def carrinho():
 def contato():
     return render_template('contato.html')
 
+# Este bloco garante que o código seja executado apenas quando o script for executado diretamente.
 if __name__ == '__main__':
     app.run(debug=True)
--- run.py
+++ run.py
@@ -1,4 +1,11 @@
 from app import app
 
+# Este bloco garante que o código abaixo seja executado apenas se este arquivo for executado diretamente,
+# Essa condição verifica se o script está sendo executado diretamente ou se está sendo importado como um módulo em outro script.
+# Apenas quando o script é executado diretamente, o servidor Flask é iniciado.
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) # "app.run" Inicia o servidor Flask. 
+    
+    # O argumento debug=Trueé muito útil durante o desenvolvimento:
+    # Atualiza o servidor automaticamente para salvar alterações.
+    # Mostra informações relacionadas a erros no navegador.
